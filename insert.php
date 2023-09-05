855eb1ae328f04ae88fce857f7aa260f5897dae2
Hasanka <amilanarangoda1998@gmail.com>
2023-09-05 09:04:21 +0530

    database-validation

--- insert.php
+++ insert.php
@@ -4,14 +4,6 @@ $username = "root";
 $password = "root";
 $databasename = "t1";
 
-// Create a connection to the database
-$con = mysqli_connect($servername, $username, $password, $databasename);
-
-// Check if the connection was successful
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 // Get the values from the form using the correct names
 $name = $_POST['name'];
 $city = $_POST['city'];
@@ -22,27 +14,51 @@ $name = trim($name);
 $city = trim($city);
 $nic = trim($nic);
 
-$crit1 = $name !="";
-$crit2 = $city !=""; 
-$crit3 = $nic !="";  
+$crit1 = $name != "";
+$crit2 = $city != "";
+$crit3 = $nic != "";
+$crit4 = strlen($name) > 3;
+$crit5 = strlen($city) > 2;
+$crit6 = strlen($nic) == 10 || strlen($nic) == 12;
 
-if(!$crit1 ) echo ('Name Is Required</br>');
-if(!$crit2 ) echo ('City Is Required</br>');
-if(!$crit3 ) echo ('NicIs Required</br>');
+if (!$crit1) echo ('Name Is Required</br>');
+if (!$crit2) echo ('City Is Required</br>');
+if (!$crit3) echo ('Nic Is Required</br>');
+if (!$crit4) echo ('Name should have more than 3 characters </br>');
+if (!$crit5) echo ('City should have more than 2 characters</br>');
+if (!$crit6) echo ('Wrong Nic</br>');
 
-if(!$crit1 || !$crit2 || !$crit3)die();
+if (!$crit1 || !$crit2 || !$crit3) die();
+
+
+// Create a connection to the database
+$con = mysqli_connect($servername, $username, $password, $databasename);
+
+//database validation
+$nicsql = "SELECT * FROM employee WHERE nic = '$nic'";
+$resultnic = mysqli_query($con, $nicsql);
+
+if (mysqli_num_rows($resultnic) > 0) {
+    exit("Sorry NIC already exists");
+}
+
 // SQL query to insert data into the 'employee' table
 $sql = "INSERT INTO employee (name, city, nic) VALUES ('$name', '$city', '$nic')";
+
+// Check if the connection was successful
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 // Perform the query and check for success
 if (mysqli_query($con, $sql)) {
     echo "Record inserted successfully.";
 } else {
-    echo "Error: " . mysqli_error($con);
+    echo "Error please enter carrect details: " . mysqli_error($con);
 }
 
 // Close the database connection
 mysqli_close($con);
 ?>
-</html>
 
+</html>
