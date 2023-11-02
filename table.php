18952b99efc682a3ae4ef275f3c972a365298712
Hasanka <amilanarangoda1998@gmail.com>
2023-11-02 20:30:51 +0530

    table_added

---
+++ table.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Table</title>
+</head>
+<body>
+    <div>
+    <h1>Students</h1>
+    <table id="" border="1px">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>CITY</th>
+                <th>NIC</th>
+            </tr>
+        </thead>
+    </table>
+    </div>
+   
+</body>
+</html>
